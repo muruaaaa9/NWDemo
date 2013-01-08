6741235d5cd21a0d0d4d2469678db272ae40ad20
Murugan Amir <murugan.amir@bglgroup.co.uk>
2013-01-08 16:03:55 +0000

    Murugan & DJ: Added entity framework classes

---
+++ Src/NWDemo.Model/Models/Products_Above_Average_Price.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace NWDemo.Model.Models
+{
+    public class Products_Above_Average_Price
+    {
+        public string ProductName { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+    }
+}
